dd39a907bab7a430ff4679ff6dba6af5260d9156
delmss <dgortega@memphis.edu>
2025-09-24 10:37:03 -0500

    saved urls

--- five/urls.py
+++ five/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include  # 👈 include is new
+from django.urls import path, include  
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("todo.urls")),  # 👈 sends the homepage (“/”) to your todo app
+    path("", include("todo.urls")),  
 ]
 
